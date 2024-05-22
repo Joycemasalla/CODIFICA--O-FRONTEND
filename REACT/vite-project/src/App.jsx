8353b0fa20c76d85b174d192e5bd142a70b824e3
joyce <0001038872@senaimgaluno.com.br>
2024-05-22 17:01:47 -0300

    Adições React

---
+++ REACT/vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+
+import Pagina2 from './Pagina2'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <Pagina2 />
+  )
+}
+
+export default App
---
+++ REACT/vite-project/src/Pagina2.jsx
@@ -0,0 +1,7 @@
+function Pagina2(){
+    return(
+    <h1>Pagina2</h1>
+    )
+
+}
+export default Pagina2
